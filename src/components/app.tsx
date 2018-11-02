bf41926f838207ba25a1f0466b4b8e1a08bc1bb9
Mathieu Rivest <mathieurivest@invisionapp.com>
2018-11-02 14:25:09 -0400

    add ts support

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -8,7 +8,8 @@ import Home from '../routes/home';
 import Profile from '../routes/profile';
 
 export default class App extends Component {
-	
+  currentUrl: string;
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,7 +1,8 @@
 import { h } from 'preact';
-import style from './style';
+// TODO support css modules
+const style = require('./style');
 
-const Home = () => (
+const Home = (props: any) => (
 	<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
