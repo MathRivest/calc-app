0b107f8f09ebfafb266e9ccb7ab54ced9f5580e1
Guillaume Salles <guillaume.salles@me.com>
2018-11-06 21:16:09 -0500

    basic addition support

---
+++ src/interpreter.tsx
@@ -0,0 +1,119 @@
+type Token = OperatorToken | NumberToken;
+
+type OperatorToken = {
+  kind: TokenKind.Operator;
+  value: string;
+};
+
+type NumberToken = {
+  kind: TokenKind.Number;
+  value: string;
+};
+
+enum TokenKind {
+  Operator,
+  Number,
+}
+
+type Node = NumberLiteral | BinaryExpression;
+
+enum NodeKind {
+  NumberLiteral,
+  BinaryExpression,
+}
+
+type NumberLiteral = {
+  kind: NodeKind.NumberLiteral;
+  value: number;
+};
+
+type BinaryExpression = {
+  kind: NodeKind.BinaryExpression;
+  left: NumberLiteral;
+  right: NumberLiteral;
+  operator: string;
+};
+
+function tokenizer(input: string): Token[] {
+  let current = 0;
+
+  const tokens: Token[] = [];
+
+  while (current < input.length) {
+    let char = input[current];
+
+    if (char === '+') {
+      tokens.push({
+        kind: TokenKind.Operator,
+        value: '+',
+      });
+      current++;
+      continue;
+    }
+
+    let NUMBERS = /[0-9]/;
+    if (NUMBERS.test(char)) {
+      let value = '';
+
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({ kind: TokenKind.Number, value });
+      continue;
+    }
+
+    throw new Error('Unkown character : ' + char);
+  }
+
+  return tokens;
+}
+
+function parser(tokens: Token[]): Node {
+  let i = 0;
+  function consumeToken(kind: TokenKind): Token {
+    const token = tokens[i];
+    if (token.kind !== kind) {
+      throw new Error(`Unexpected token : ${token}`);
+    }
+    i++;
+    return token;
+  }
+
+  function makeNumberLiteral(token: NumberToken): NumberLiteral {
+    return {
+      kind: NodeKind.NumberLiteral,
+      value: parseInt(token.value),
+    };
+  }
+
+  const left = consumeToken(TokenKind.Number) as NumberToken;
+  const operator = consumeToken(TokenKind.Operator) as OperatorToken;
+  const right = consumeToken(TokenKind.Number) as NumberToken;
+
+  return {
+    kind: NodeKind.BinaryExpression,
+    left: makeNumberLiteral(left),
+    operator: operator.value,
+    right: makeNumberLiteral(right),
+  };
+}
+
+function evaluate(ast: Node): string {
+  if (ast.kind !== NodeKind.BinaryExpression) {
+    throw new Error(`Invalid ast node: ${ast}`);
+  }
+
+  if (ast.operator !== '+') {
+    throw new Error(`Invalid operator: ${ast.operator}`);
+  }
+
+  return (ast.left.value + ast.right.value).toString();
+}
+
+export default function(input: string): string {
+  const tokens = tokenizer(input);
+  const ast = parser(tokens);
+  return evaluate(ast);
+}
