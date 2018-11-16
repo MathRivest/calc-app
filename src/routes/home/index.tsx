9d430814486f7ec2f4ff364d1f2e6208feaaa86e
Mathieu Rivest <mathieurivest@invisionapp.com>
2018-11-16 17:37:33 -0500

    remove unused files

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,9 +1,6 @@
 import { h } from 'preact';
 import Calculator from '../../components/Calculator/Calculator';
 
-// TODO support css modules
-const style = require('./style');
-
 const Home = (_props: any) => {
   return <Calculator />;
 };
