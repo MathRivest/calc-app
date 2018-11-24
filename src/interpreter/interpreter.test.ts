7477b774e92c5ae3db9321cea7f338583aaf03ce
Guillaume Salles <guillaume.salles@me.com>
2018-11-24 15:03:07 -0500

    support number representation at sublevel (#15)
    * support number representation at sublevel
    
    * update tests
    
    * minify tests
    
    * float support (#16)

--- src/interpreter/interpreter.test.ts
+++ src/interpreter/interpreter.test.ts
@@ -1,56 +1,26 @@
 import interpreter from './interpreter';
 
-test('addition', () => {
-  expect(interpreter('1+2')).toBe('3');
-});
+function t(expression: string, expected: string) {
+  test(`${expression} => ${expected}`, () => {
+    expect(interpreter(expression)).toBe(expected);
+  });
+}
 
-test('substraction', () => {
-  expect(interpreter('2-1')).toBe('1');
-});
-
-test('multiplication', () => {
-  expect(interpreter('2+1*3')).toBe('5');
-});
-
-test('division', () => {
-  expect(interpreter('10-2/4')).toBe('9.5');
-});
-
-test('parenthesis', () => {
-  expect(interpreter('(10-2)/4*(3+1)')).toBe('8');
-});
-
-test('unary plus and minus', () => {
-  expect(interpreter('-(10-2)+(+3)')).toBe('-5');
-});
-
-test('exponent', () => {
-  expect(interpreter('2^3')).toBe('8');
-  expect(interpreter('2^3^4')).toBe('4096');
-  expect(interpreter('2*2^3')).toBe('16');
-});
-
-test('white space', () => {
-  expect(interpreter('  ( 2 + 3 )  ')).toBe('5');
-});
-
-test('plus', () => {
-  expect(interpreter('1plus2')).toBe('3');
-  expect(interpreter('1 plus 2')).toBe('3');
-});
-
-// Todo: Handle composite keywords
-test.skip('divide by', () => {
-  expect(interpreter('4 divide by 2')).toBe('2');
-});
-
-describe('binary', () => {
-  test('number to binary', () =>
-    expect(interpreter('5 in binary')).toBe('0b101'));
-  test('expression to binary', () =>
-    expect(interpreter('1+2+3 in binary')).toBe('0b110'));
-
-  // Todo
-  test.skip('number representation propagation', () =>
-    expect(interpreter('1 in binary + 6')).toBe('0b111'));
-});
+t('1+2', '3');
+t('2-1', '1');
+t('2+1*3', '5');
+t('10-2/4', '9.5');
+t('(10-2)/4*(3+1)', '8');
+t('-(10-2)+(+3)', '-5');
+t('2^3', '8');
+t('2^3^4', '4096');
+t('2*2^3', '16');
+t('  ( 2 + 3 )  ', '5');
+t('1plus2', '3');
+t('1 plus 2', '3');
+// t('4 divide by 2','2'); Todo: Handle composite keywords
+t('5 in binary', '0b101');
+t('1+2+3 in binary', '0b110');
+t('1 in binary + 6', '0b111');
+// t('0b101 in decimal', '5'); Todo: Handle conversion to decimal
+t('1.5+0.33', '1.83');
