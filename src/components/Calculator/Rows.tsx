7dc085739c5daecb90f04b35677026ef46b2ce4d
Mathieu Rivest <rivest.mathieu@gmail.com>
2018-11-24 18:50:09 -0500

    update row item

--- src/components/Calculator/Rows.tsx
+++ src/components/Calculator/Rows.tsx
@@ -1,4 +1,4 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import Interpreter from '../Interpreter';
 const style = require('./Rows.css');
 
@@ -10,13 +10,22 @@ export default function Rows({ expressions }: IRowsProps) {
   return (
     <div className={style.rows}>
       {expressions.map(expression => (
-        <div className={style.rowItem}>
-          <div className={style.rowItemValue}>{expression}</div>
-          <div className={style.rowItemResult}>
-            <Interpreter value={expression} />
-          </div>
-        </div>
+        // <RowItem expression={expression}>
+        <Interpreter value={expression} />
       ))}
     </div>
   );
 }
+
+// class RowItem extends Component<any, any> {
+//   render({ expression }) {
+//     return (
+//       <div className={style.rowItem}>
+//         <div className={style.rowItemValue}>{expression}</div>
+//         <div className={style.rowItemResult}>
+//           <Interpreter value={expression} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
